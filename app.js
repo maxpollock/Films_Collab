18a6e409d80f466751a28366a8a68d55509a8c5a
MAX POLLOCK <mcpollock00@gmail.com>
2023-09-15 14:36:00 +0100

    Add JS functions. Add CSS animations and styling. Added additional prompts with while/for statements.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Asking user their name for input
+
 function writeMessage() {
   let username = prompt("Hello there! What is your name?");
 
@@ -10,7 +12,9 @@ function writeMessage() {
   document.write(userMessage);
 }
 
-function buttonOutcome() {
+// Confirm function to say whether they prefer watching movies at home or at cinema
+
+function viewQuestion() {
   let text;
 
   if (
@@ -23,11 +27,43 @@ function buttonOutcome() {
     text =
       "Awesome! You prefer going to the big screen cinema. Where's the popcorn?";
   }
+  return text;
+}
 
+// Doc write the outcome of viewQuestion function
+
+function questionOutcome() {
   document.write(text);
 }
+
+// Change background function
 
 function chgBgd() {
   let color = prompt("Let's change the background! Give me a color!");
   document.body.style.backgroundColor = color;
+}
+
+// Popcorn Img generation question
+
+function givePopcorn() {
+  let howMany = prompt(
+    "How many tubs of Popcorn would you like? I have 5 avaliable.",
+    5
+  );
+
+  while (!(howMany > 0 && howMany <= 5)) {
+    howMany = prompt("I only have 5 tubs. Please choose between 1 - 5.");
+  }
+
+  for (let i = 0; i < howMany; i++) {
+    document.write("<img src='./images/popcorn.png'/>");
+  }
+}
+
+function inputNumber() {
+  let numBer = prompt("Please input a number between 1 and 5.");
+
+  while (numBer >= 6 && number < 0) {
+    numBer = prompt("That is incorrect. Please input a number between 1 - 5.");
+  }
 }
